28c60fc009c6e0851c29999f708585fd0a79dd41
E24056768 <jj851123@gmail.com>
2017-03-31 01:00:03 +0800

    lab2

---
+++ lab2.h
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <fstream> 
+#include <string>
+#include <cstdlib> 
+#include<cmath>
+using namespace std;
+class Record{
+public:
+	Record();
+	
+	void setStart(double a,double b,double c);
+	int getStartA(double s1);
+	int getStartB(double s2);
+	
+private:
+	double K,RA,RB;
+};
